a4e59b2bea479ec981965202225a0b4a446baaaa
Peter Burkholder <peter.burkholder@audaxhealth.com>
2013-06-11 12:40:26 -0400

    Adding working stub

---
+++ aws_count/count_instances.rb
@@ -0,0 +1,26 @@
+#!/usr/local/bin/ruby
+
+require 'yaml'
+require 'aws-sdk'
+
+config_file = File.join(File.dirname(__FILE__), 'config.yml')
+
+config = YAML.load(File.read(config_file))
+
+AWS.config(config)
+
+c = AWS::EC2::ReservedInstancesCollection.new()
+
+zones=('a'..'e').to_a.map{|z| "us-east-1#{z}"}
+
+# The filter methods use filter names based on the API # docs, e.g.
+# http://docs.aws.amazon.com/AWSEC2/latest/APIReference/ApiReference-query-DescribeReservedInstances.html
+# not the Object attributes in the AWS Ruby SDK. Hence, we filter on
+# 'instance-type', not 'instance_type'
+
+zones.each do |zone|
+  printf "|| %s || Reserved# || Running #||\n", zone
+end
+
+c.filter('state', 'active').filter('instance-type', 'm1.large') \
+  .each{|i| puts i.instance_count}
